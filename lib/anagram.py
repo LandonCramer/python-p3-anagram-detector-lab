55e5076d4e76964baa432d6043127f987bfc987a
Landon <landoncramer1@gmail.com>
2023-10-28 16:39:07 -0700

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  # Convert the word to lowercase for case-insensitive comparison
+
+    def match(self, candidates):
+        anagrams = []
+      
+        for candidate in candidates:
+            candidate = candidate.lower().replace(" ", "")
+            
+            if candidate != self.word and sorted(candidate) == sorted(self.word):
+                anagrams.append(candidate)
+
+        return anagrams
